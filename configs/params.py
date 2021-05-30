f5daa52ee714e209e232c8b85a218c02c974aa4b
zwzhang <58326593+zwzhangzzz@users.noreply.github.com>
2021-05-30 22:41:18 +0800

    params file

---
+++ configs/params.py
@@ -0,0 +1,29 @@
+class nyc_params:
+    len_recent_time = 5
+    number_region = 64
+    threshold_nc = "threshold_nc.npy"
+    label = 'label.npy'
+    all_data = 'data_nyc.npy'
+    dict_xy = "dict_xy.npy"
+    learning_rate = 0.001
+    dr = 58
+    number_sp = 6
+    patience = 10
+    delta = 0
+    batch_size = 25
+    training_epoch = 100
+
+class chicago_params:
+    len_recent_time = 5
+    number_region = 27
+    threshold_nc = "threshold_nc.npy"
+    label = 'label.npy'
+    all_data = 'data_chicago.npy'
+    dict_xy = "dict_xy.npy"
+    learning_rate = 0.001
+    dr = 58
+    number_sp = 6
+    patience = 10
+    delta = 0
+    batch_size = 25
+    training_epoch = 100
